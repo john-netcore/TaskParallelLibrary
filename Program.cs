88309c4bbebb8f6bbbe40f18e47dc5ef18c570aa
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-02 19:09:57 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TaskParallelLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press key to quit.");
+            Console.ReadKey();
+        }
+    }
+}
