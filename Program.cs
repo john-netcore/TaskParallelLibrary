e023eff498fabe5e2505abd00fbe17362f9c23dd
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-02 19:54:16 +0200

    Created and started multiple tasks.

--- Program.cs
+++ Program.cs
@@ -10,9 +10,13 @@ namespace TaskParallelLibrary
         {
             //Declaration of task one. The task does not do anything until it gets started.
             var task1 = new Task(() => DoSomeVeryImportantWork(1, 2000));
+            var task2 = new Task(() => DoSomeVeryImportantWork(2, 3000));
+            var task3 = new Task(() => DoSomeVeryImportantWork(3, 1000));
 
             //Starts to execute the task operations.
             task1.Start();
+            task2.Start();
+            task3.Start();
 
             //This code continues to execute in parallel with the task operations.
             Console.WriteLine("Press key to quit.");
