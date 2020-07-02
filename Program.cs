60faf300573b5067d5a74928548ccb7740c8b921
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-02 19:15:14 +0200

    Created Task 1.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TaskParallelLibrary
 {
@@ -6,6 +8,14 @@ namespace TaskParallelLibrary
     {
         static void Main(string[] args)
         {
+            //Declaration of task one. The task does not do anything until it gets started.
+            var task1 = new Task(() =>
+            {
+                System.Console.WriteLine("Task 1 is beginning.");
+                Thread.Sleep(2000);
+                System.Console.WriteLine("Task 1 has completed.");
+            });
+
             Console.WriteLine("Press key to quit.");
             Console.ReadKey();
         }
