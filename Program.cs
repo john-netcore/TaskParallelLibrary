3b1694eee07cbe2f68a1f2374d1e8827049e786a
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-03 10:50:43 +0200

    Using Parallel.For

--- Program.cs
+++ Program.cs
@@ -11,8 +11,15 @@ namespace TaskParallelLibrary
         {
             var workToDo = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 
+            System.Console.WriteLine("Running list content in parallel:");
+
             //Executing Actions in parallel, Parallel blocks the code until it is done with all the operations.
             Parallel.ForEach(workToDo, i => System.Console.WriteLine(i));
+
+            System.Console.WriteLine("Running numbers between 1 and 100 in parallel:");
+
+            //Runs an action for all the numbers between 1 and 100 in parallel.
+            Parallel.For(1, 100, i => System.Console.Write("{0} ", i));
 
             Console.WriteLine("Press key to quit.");
             Console.ReadKey();
