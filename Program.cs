6ee4c44f2462f27d357f919a0d0543ce32cc68f6
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-03 11:25:18 +0200

    Using Cancellation tokens to cancel tasks.

--- Program.cs
+++ Program.cs
@@ -9,31 +9,44 @@ namespace TaskParallelLibrary
     {
         static void Main(string[] args)
         {
-            var workToDo = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            var source = new CancellationTokenSource();
 
-            System.Console.WriteLine("Running list content in parallel:");
+            try
+            {
+                Task.Factory.StartNew(() => DoSomeVeryImportantWork(1, 2000, source.Token))
+                                .ContinueWith((prevTask) => DoSomeOtherVeryImportantWork(1, 1500, source.Token));
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.GetType());
+            }
 
-            //Executing Actions in parallel, Parallel blocks the code until it is done with all the operations.
-            Parallel.ForEach(workToDo, i => System.Console.WriteLine(i));
-
-            System.Console.WriteLine("Running numbers between 1 and 100 in parallel:");
-
-            //Runs an action for all the numbers between 1 and 100 in parallel.
-            Parallel.For(1, 100, i => System.Console.Write("{0} ", i));
+            Thread.Sleep(500);
+            source.Cancel();
 
             Console.WriteLine("Press key to quit.");
             Console.ReadKey();
         }
 
-        static void DoSomeVeryImportantWork(int id, int sleepTime)
+        static void DoSomeVeryImportantWork(int id, int sleepTime, CancellationToken token)
         {
+            if (token.IsCancellationRequested)
+            {
+                System.Console.WriteLine("Cancellation requested.");
+                token.ThrowIfCancellationRequested();
+            }
             System.Console.WriteLine("Task {0} is beginning.", id);
             Thread.Sleep(sleepTime);
             System.Console.WriteLine("Task {0} has completed.", id);
         }
 
-        static void DoSomeOtherVeryImportantWork(int id, int sleepTime)
+        static void DoSomeOtherVeryImportantWork(int id, int sleepTime, CancellationToken token)
         {
+            if (token.IsCancellationRequested)
+            {
+                System.Console.WriteLine("Cancellation requested.");
+                token.ThrowIfCancellationRequested();
+            }
             System.Console.WriteLine("Task {0} is beginning other work.", id);
             Thread.Sleep(sleepTime);
             System.Console.WriteLine("Task {0} has completed other work.", id);
